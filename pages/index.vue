3123b6243966527713771a3015310a21d749dced
rob <jcruda@yondu.com>
2023-09-11 03:28:19 +0800

    Vip Exlusives
    Signed-off-by: rob <jcruda@yondu.com>

--- pages/index.vue
+++ pages/index.vue
@@ -11,13 +11,16 @@
     <section>
       <SiteWideDeals/>
     </section>
+
+    <section>
+      <VipExclusive/>
+    </section>
   </div>
 </template>
 
 <style lang="scss" scoped>
 .test {
   @apply relative;
-  height: 200vh;
   margin-top: 93px;
 }
 </style>
